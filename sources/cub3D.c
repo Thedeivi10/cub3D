aa0ec3dd40cc7d7d4777dca24e9925f210bc1b52
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2025-05-31 14:24:46 +0200

    continuing parse

--- sources/cub3D.c
+++ sources/cub3D.c
@@ -6,11 +6,23 @@
 /*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 17:21:35 by davigome          #+#    #+#             */
-/*   Updated: 2025/05/30 19:32:23 by davigome         ###   ########.fr       */
+/*   Updated: 2025/05/31 08:59:06 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
+
+void	ft_read_map(t_map *game, char **argv)
+{
+	char	*line;
+	int		fd;
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		fprintf()
+	}
+}
 
 void	ft_check_name(char **argv)
 {
@@ -18,9 +30,9 @@ void	ft_check_name(char **argv)
 
 	len = ft_strlen(argv[1]);
 	if (argv[1][len - 1] != 'b' ||  argv[1][len - 2] != 'u'
-		|| argv[1][len - 3] != 'c' || argv[1][len - 2] != '.')
+		|| argv[1][len - 3] != 'c' || argv[1][len - 4] != '.')
 	{
-		fprintf(stderr, "Error\nThe map must end with .cub");
+		fprintf(stderr, "Error\nThe map must end with .cub.\n");
 		exit(1);
 	}
 }
@@ -28,6 +40,7 @@ void	ft_check_name(char **argv)
 void	ft_checks(t_map *game, char **argv)
 {
 	ft_check_name(argv);
+	ft_read_map(game, argv);
 }
 
 int main(int argc, char **argv)
